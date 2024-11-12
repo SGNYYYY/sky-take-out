1885dd306edb2016323fc75ba74e923d3faa60aa
SGNYYYY <shangguanningyuan@gmail.com>
2024-11-12 19:14:43 +0800

    文件上传、新增菜品代码实现

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,6 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -16,4 +19,11 @@ public interface DishMapper {
      */
     @Select("select count(*) from dish where category_id=#{categoryId}")
     Integer countByCategoryId(Long categoryId);
+
+    /**
+     * 新增菜品
+     * @param dish
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
